a18623900c3aa3096d348321135f5d7707d56c51
Luke Minall <luke@lukeminall.co.uk>
2019-06-01 14:28:19 +0100

    added test file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Sites Folder</h1>
+    <p><?php phpinfo(); ?></p>
+    <p><?php echo $_SERVER['HTTP_USER_AGENT']; ?></p>
+  </body>
+</html>
