7036382da91c53da9fa89cb388f781153f0b58fd
Luke Minall <luke@lukeminall.co.uk>
2019-06-03 23:59:03 +0100

    added booleans example@

---
+++ 04-booleans/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      // set boolean value.
+      $boolean = true;
+
+      // print statment depending on boolean, 1 is true & 0 is false.
+      if ($boolean == 1) {
+        echo "boolean is true";
+      } else{
+        echo "boolean is false";
+      }
+     ?>
+  </body>
+</html>
