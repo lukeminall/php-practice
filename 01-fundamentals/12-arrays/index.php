468bfac8551e129ce19f0d5c327f4cc96efbb4aa
Luke Minall <luke@lukeminall.co.uk>
2019-06-18 00:47:26 +0100

    added arrays example

---
+++ 01-fundamentals/12-arrays/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Arrays</title>
+  </head>
+  <body>
+    <h1>2019 Conservative Leadership Contest</h1>
+    <?php
+      $candidates = array("Boris Johnson","Jeremy Hunt","Michael Gove","Dominic Raab","Sajid Javid","Rory Stewart");
+
+      echo "<p>The current favourite is ".$candidates[0]."<p>";
+     ?>
+  </body>
+</html>
